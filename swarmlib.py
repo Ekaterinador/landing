89a05c78229597964dbf31935b046b31e0f1f8f7
EkaterinaDor <katydor1531@mail.ru>
2022-04-26 21:05:50 +0300

    clean

--- swarmlib.py
+++ swarmlib.py
@@ -24,31 +24,17 @@ from crazyswarm.msg import Position
 from  crazyswarm.msg import body_pos
 from multiprocessing import Process
 import os
-# from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import NearestNeighbors
 
 np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
 
 
 # Main classes ####################################################################
 
-class swarm_manager():
-    def __init__(self, drone_name_list):
-        self.drone_name_list = drone_name_list
-
-        drone_object_list = []
-        for drone_name in self.drone_name_list:
-            drone_object = drone(drone_name)
-            drone_object_list.append(drone_object)
-        return drone_object_list
-
-    def update_position_for_all(self, drone_object_list):
-        for drone_object in drone_object_list:
-            drone_object.position()
-
-
 class Drone:
-    def __init__(self, name, shift, leader=False):
+    def __init__(self, name, shift, cf, leader=False):
         self.name = name
+        self.cf = cf
         # self.tf = '/vicon/' + name + '/' + name
         self.tf = '/' + name
         self.leader = leader
@@ -58,14 +44,15 @@ class Drone:
         self.sp = np.array([0, 0, 0])
         self.path = Path()
         self.obstacle_update_status = [False, None]
-        self.rad_imp = radius_impedance_model()
-        self.pos_goal = np.array([0.0, 0, 0.7])
+        self.pos_goal = np.array([1.0, 0, 0.7])
         self.shift = shift
         self.file = np.array([0, 0, 0])
         self.file_goal = np.array([0, 0, 0])
         self.yaw = 0.0
         self.cam_coord = np.array([0.0, 0., 0.0])
         self.swarm = []
+        self.land_request = 0
+        self.shutdown = False
 
 
     def position(self):
@@ -73,7 +60,7 @@ class Drone:
         position, quaternion = self.tl.lookupTransform("/world", self.tf, rospy.Time(0))
         self.pose = position
         self.file = np.vstack([self.file, self.pose])
-        np.save(self.name , self.file)
+        np.save(self.name, self.file)
         return np.array(position)
 
     def orientation(self):
@@ -90,7 +77,6 @@ class Drone:
         print(name)
         print(msg)
 
-
     def publish_sp(self, orient):
         publish_pose(self.sp, orient, self.name + "_sp")
 
@@ -104,10 +90,7 @@ class Drone:
         publish_goal_pos(self.sp, self.yaw, "/" + self.name)
         print('publish ', self.sp)
 
-    def update_radius_imp(self, delta):
-        if self.rad_imp.inside:
-            self = impedance_obstacle_delta(self, delta)
-            self.sp += self.rad_imp.pose
+
 
     def follower(self, pos_goal):
         print('pos goal ', pos_goal)
@@ -117,28 +100,27 @@ class Drone:
         dz = pos_goal[2] + self.shift[2] - self.pose[2]
         target_force = np.array([dx, dy, dz])
 
-        cf_id = swarm.index(self)
-        position_table = np.zeros((len(swarm), 3))
-        for i in range(len(swarm)):
-            position_table[i] = tuple(swarm[i].pos[j][0] for j in range(3))
-        distances, neighbors = get_neighbors(position_table, len(swarm), min_dist)
-        sep_force = self.separation_force(neighbors[cf_id], distances[cf_id], 2)
-        target_force = target_force + sep_force
 
-        self.sp = self.pose + limit_speed(target_force, 0.3, 0.3)
+
+        if np.linalg.norm(target_force) < 0.15:
+            if self.land_request == 1:
+                self.shutdown = 1
+                print(self.name, ' shutdown')
+            self.land_request = 1
+        self.sp = self.pose + limit_speed(target_force, 0.5, 0.5)
         print('sp ', self.sp)
 
     def cam_data(self, msg):
         pos = msg.transform.translation
         print('pos from camera ', pos)
        # vertical
-       #  x = pos.z
-       #  y = pos.x
-       #  z = pos.y
-        # horizontal
-        x = pos.y
+        x = pos.z
         y = pos.x
-        z = - pos.z
+        z = pos.y
+        # horizontal
+        # x = pos.y
+        # y = pos.x
+        # z = - pos.z
         V = cam_rotation(np.array([x, y, z]), self.yaw)
         print('V ', V)
         self.cam_coord = V
@@ -149,43 +131,6 @@ class Drone:
         # self.yaw += math.atan(x/y)
         print('GOAL_POS: ', self.pos_goal)
 
-    def potentional_field(self, eps=EPS, min_dist=MIN_DIST, k_separation=K_SEPARATION, k_target=K_TARGET, rate=RATE,
-                          lim_up=LIM_UP, lim_down=LIM_DOWN):
-        print('start pot field')
-        cf_id = self.swarm.index(self)
-        print(self.swarm[0].pos)
-        position_table = np.zeros((len(self.swarm), 3))
-        for i in range(len(self.swarm)):
-            position_table[i] = tuple(self.swarm[i].pos[j][0] for j in range(3))
-
-        distances, neighbors = get_neighbors(position_table, len(self.swarm), min_dist)
-        sep_force = self.separation_force(neighbors[cf_id], distances[cf_id], k_separation)
-
-        tar_force = self.target_force(k_target)
-        speed = tar_force + sep_force
-        self.vel = limit_speed(speed, lim_up, lim_down)
-        self.pos = self.pos + self.vel / rate
-
-    def separation_force(self, neighbors, distances, k_separation):
-        sep_sum = np.zeros((3, 1))
-
-        if np.shape(neighbors)[0] == 1:
-            return np.zeros((3, 1))
-
-        for indx in range(1, len(neighbors)):
-            dist = distances[indx]
-            if dist > 1e-3:
-                dir = self.pos - self.swarm[neighbors[indx]].pos
-                sep = dir / dist  # the smaller the distance, the greater the force
-                sep_sum = np.add(sep_sum, sep)
-
-        sep_sum = k_separation * sep_sum
-        return sep_sum
-
-
-    def target_force(self, k_target):
-        target_force =  k_target * (self.pos_goal - self.pos)
-        return target_force
 
 def cam_rotation(V, yaw):
     R_yaw = np.array([[np.cos(yaw), - np.sin(yaw), 0],
@@ -264,6 +209,24 @@ def msg_def_crazyswarm(pose, yaw):
     msg.y = pose[1]
     msg.z = pose[2]
     msg.yaw = yaw
+    now = rospy.get_time()
+    msg.header.seq = 0
+    msg.header.stamp = rospy.Time.now()
+    return msg
+
+
+def msg_def_roboarm(cmd_vel):
+    worldFrame = rospy.get_param("~worldFrame", "/world")
+    msg = Position()
+    msg.header.seq = 0
+    msg.header.stamp = rospy.Time.now()
+    msg.header.frame_id = worldFrame
+    print('cmd_vel ', cmd_vel)
+    msg.x = cmd_vel[0]
+    msg.y = cmd_vel[1]
+    msg.z = cmd_vel[2]
+    print('x, y, z ', msg.x, msg.y, msg.z)
+    msg.yaw = 0
     now = rospy.get_time()
     msg.header.seq = 0
     msg.header.stamp = rospy.Time.now()
